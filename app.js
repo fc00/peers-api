fa14d7ff35948145a107d25b5d05afa026d3833c
ansuz <ansuz@transitiontech.ca>
2016-10-01 11:27:48 +0200

    sort of working location query

--- app.js
+++ app.js
@@ -1,27 +1,54 @@
 var Peers = require("hyperboria-peers");
 
+var identity = function (x) { return x; };
+
+var parseReq = function (req) {
+    return req.url.split(/\//).filter(identity);
+};
+
 var asJSON = function (res, json) {
     res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(json, null, 2));
+    res.end(JSON.stringify(json, null, 2) + '\n');
 };
+
+var routes = [
+    [
+        '/1/location/<continent>',
+    ]
+];
 
 module.exports = function (Config) {
     return function (req, res) {
-        var send = function (data) {
-            asJSON(res, data);
+        var send = function (data) { asJSON(res, data); };
+        var home = function () {
+            send(routes[0]);
         };
 
-        switch (req.url) {
-            case '/all':
-                send(Peers.peers);
-                break;
-            default:
-                send({
-                    options: [
-                        '/all',
-                    ]
-                });
+        var argv = parseReq(req);
+        console.log(argv);
+
+        var apiVersion;
+
+        if (argv.length === 0) { return home(); }
+        if (argv.length >= 1) {
+            apiVersion = parseInt(argv[0]);
+            if (isNaN(apiVersion)) {
+                return home();
+            }
         }
+
+        // if you get here, it's not the home route, and there's a valid version
+        var handleVersion1 = function (args) {
+            if (args[1] && args[0] === 'location') {
+                return send({
+                    result: Peers.filter(function (x, p) {
+                        return p.indexOf(args[1]) !== -1;
+                    })
+                });
+            }
+        };
+
+        handleVersion1(argv.slice(1));
     };
 };
 
